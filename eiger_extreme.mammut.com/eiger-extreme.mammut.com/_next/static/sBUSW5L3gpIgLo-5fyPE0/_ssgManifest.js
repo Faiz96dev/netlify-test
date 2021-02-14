10d4e1b9acba90c4cb5f12e3f2ea3c59375ec8b7
Fazyr <46592131+Faiz96dev@users.noreply.github.com>
2021-02-14 10:40:49 +0600

    Delete eiger_extreme.mammut.com directory

--- eiger_extreme.mammut.com/eiger-extreme.mammut.com/_next/static/sBUSW5L3gpIgLo-5fyPE0/_ssgManifest.js
+++
@@ -1,2 +0,0 @@
-self.__SSG_MANIFEST = new Set(["\u002F[locale]\u002Ftechnology", "\u002F[locale]", "\u002F[locale]\u002Fwatch", "\u002F[locale]\u002Fdevelopment"]);
-self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB()
